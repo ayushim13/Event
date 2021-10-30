35133273dbf72ebe7b2a170c27ad3717aa0df565
Arpit <111arpit1@gmail.com>
2021-10-31 00:21:38 +0530

    e

---
+++ js/scroll.js
@@ -0,0 +1,32 @@
+//Get the button
+var mybutton = document.getElementById("scroll");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+function enter(element) {
+  element.children[1].style.display = 'block';
+
+}
+
+function exit(element) {
+  element.children[1].style.display = 'none';
+
+}
